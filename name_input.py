3fc7b3e8d72e4317e401575e614d7669483fa2c5
mystickoder <89115482+mystickoder@users.noreply.github.com>
2023-01-31 22:52:15 -0500

    Name Entry
    Simple name entery

---
+++ name_input.py
@@ -0,0 +1,8 @@
+first_name = input ("What is your first name? ")
+last_name = input ("What is your last name? ")
+age = input("How old are you? ")
+
+greeting = (f"Hello {first_name} {last_name}, I see you are {age}. Welcome to the club.")
+
+
+print(greeting)
